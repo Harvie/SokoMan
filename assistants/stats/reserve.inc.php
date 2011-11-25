bfb8a227e1ea00ba0e3397d6155acdf2f1e3b706
Thomas Mudrunka <tomas@mudrunka.cz>
2011-11-25 02:15:52 +0100

    Opravena haluz s wishlistem

--- assistants/stats/reserve.inc.php
+++ assistants/stats/reserve.inc.php
@@ -1,6 +1,11 @@
 <?php
 $queries += array(
-	"Nutno koupit"
+	"Nakoupit: Došlo úplně (Urgent WishList)"
+		=> 'SELECT model_id,model_name,model_barcode,model_reserve,model_reserve as item_quantity_to_buy'.
+		' FROM model'.
+		' WHERE model_reserve>0 AND NOT EXISTS'.
+		' (SELECT item_id FROM item WHERE model.model_id=item.model_id AND status_id=1)',
+	"Nakoupit: Dochází (WishList)"
 		=> 'SELECT room_id,room_name,model_id,model_name,model_barcode,model_reserve,'.
 		' COUNT(item_id),SUM(item_quantity),model_reserve-SUM(item_quantity) as item_quantity_to_buy'.
 		' FROM item LEFT JOIN model USING(model_id) LEFT JOIN room USING(room_id)'.
--- assistants/stats/stock.inc.php
+++ assistants/stats/stock.inc.php
@@ -1,6 +1,6 @@
 <?php
 $queries += array(
-	"Ceník + Počet kusů skladem"
+	"Ceník + Počet kusů skladem (PriceList + Stock)"
 		=> 'SELECT room_id,room_name,model_id,model_name,model_barcode,model_price_out,COUNT(item_id),SUM(item_quantity)'.
 		' FROM item LEFT JOIN model USING(model_id) LEFT JOIN room USING(room_id)'.
 		' WHERE item_valid_till=0 AND status_id=1'.
