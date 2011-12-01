1feeabc6851a1f7e6a8d6b159638f912fdea646c
Thomas Mudrunka <tomas@mudrunka.cz>
2011-12-01 14:39:48 +0100

    V ceniku se nezobrazuji blbosti

--- apis/price-list.inc.php
+++ apis/price-list.inc.php
@@ -1,5 +1,5 @@
 <?php
-$sql = 'SELECT model_name,model_price_out FROM model';
+$sql = 'SELECT model_name,model_price_out FROM model WHERE model_price_out > 0 AND category_id > 0';
 $result = $this->db->safe_query_fetch($sql);
 foreach($result as $item) $items[$item['model_name']]=$item['model_price_out'];
 die(json_encode($items));
