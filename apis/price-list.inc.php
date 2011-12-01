ae8c33eca76fc539e27d79f5b30b46cfcf1aac16
Thomas Mudrunka <tomas@mudrunka.cz>
2011-12-01 15:54:24 +0100

    API s cenikem je nyni komplexnejsi (a ma kratsi kod :-)

--- apis/price-list.inc.php
+++ apis/price-list.inc.php
@@ -1,5 +1,6 @@
 <?php
-$sql = 'SELECT model_name,model_price_out FROM model WHERE model_price_out > 0 AND category_id > 0';
+$sql = 'SELECT model_name,model_price_out,producer_name'.
+	' FROM model LEFT JOIN producer USING(producer_id)'.
+	' WHERE model_price_out > 0 AND category_id > 0';
 $result = $this->db->safe_query_fetch($sql);
-foreach($result as $item) $items[$item['model_name']]=$item['model_price_out'];
-die(json_encode($items));
+die(json_encode($result));
