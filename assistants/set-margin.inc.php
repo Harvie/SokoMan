19cbf1cb4937423b899c110a020c3399f4e5032e
Thomas Mudrunka <tomas@mudrunka.cz>
2011-12-14 05:29:11 +0100

    Assistent na nastaveni prirazky je nyni hezci a bezpecnejsi

--- assistants/set-margin.inc.php
+++ assistants/set-margin.inc.php
@@ -1,14 +1,17 @@
 <?php
 switch($SUBPATH[0]) {
 	default: case 1:
-		echo $this->html->form("$URL/2", 'GET', array(
+		echo $this->html->form("$URL/2", 'POST', array(
 			array('margin','40','text',false,'autofocus','margin (%):'),
 			array(false,'SET-MARGIN','submit')
 		));
 		break;
 	case 2:
-		$margin = 1+($_GET['margin']/100);
-		$this->db->safe_query("UPDATE model SET model_price_out = CEIL(model_price_in * $margin)");
-		echo(T('Margin set to').' '.($margin*100-100).'%');
+		if(isset($_POST['margin'])) {
+			$margin = 1+($_POST['margin']/100);
+			$this->db->safe_query("UPDATE model SET model_price_out = CEIL(model_price_in * $margin)");
+			$this->post_redirect_get("$URL_INTERNAL/1", T('Margin set to').' '.($margin*100-100).'%');
+		}
+		$this->post_redirect_get("$URL_INTERNAL/1","Set-margin: No value passed!", true);
 		break;
 }
