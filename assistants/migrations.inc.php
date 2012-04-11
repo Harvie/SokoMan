f7740b1c92b980d3529043d2e49798f5e0cc919f
Thomas Mudrunka <tomas@mudrunka.cz>
2012-04-11 13:56:35 +0200

    kosmeticky vylepseni migracnich skriptu

--- assistants/migrations.inc.php
+++ assistants/migrations.inc.php
@@ -1,8 +1,9 @@
 <pre>
-<h1>populate columns item_date_bought and item_date_sold</h1><?php
+<h1>-- populate columns item_date_bought and item_date_sold</h1><?php
 $data = $this->db->safe_query_fetch('SELECT item_id,MIN(item_valid_from) AS min,MAX(item_valid_from) AS max FROM item GROUP BY item_id;');
 //print_r($data);
 echo("START TRANSACTION;\n");
 foreach($data as $line) echo("UPDATE item SET item_date_bought='".$line['min']."',item_date_sold='".$line['max']."' WHERE item_id=".$line['item_id'].";\n");
 echo("COMMIT;\n");
 ?>
+</pre>
