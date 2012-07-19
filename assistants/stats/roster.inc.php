301e9536557644c6b895e109c2d285443459d4bc
Thomas Mudrunka <tomas@mudrunka.cz>
2012-07-19 19:48:28 +0200

    Roster s podporou audit-trails

--- assistants/stats/roster.inc.php
+++ assistants/stats/roster.inc.php
@@ -1,5 +1,5 @@
 <?php
 $queries += array(
 	"Skladová soupiska (Warehouse roster)"=>
-		"select model_id, model_name, group_concat(item_serial separator ', ') from item LEFT JOIN model USING(model_id) GROUP BY model_id",
+		"select model_id, model_name, group_concat(item_serial separator ', ') from item LEFT JOIN model USING(model_id) WHERE item_valid_till=0 AND status_id=1 GROUP BY model_id",
 );
