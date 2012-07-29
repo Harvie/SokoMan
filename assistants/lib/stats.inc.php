e19f08cf3865593cb1dbc4458cd125361bd74f01
Thomas Mudrunka <tomas@mudrunka.cz>
2012-07-29 21:15:58 +0200

    Statistiky odted nejsou fatalni

--- assistants/lib/stats.inc.php
+++ assistants/lib/stats.inc.php
@@ -2,7 +2,7 @@
 $queries = array(); //TODO: use build_query_select()!!!
 foreach($stats as $stat) include("assistants/stats/$stat.inc.php");
 foreach($queries as $description => $sql) {
-	$result = $this->db->safe_query_fetch($sql);
+	$result = $this->db->safe_query_fetch($sql,false);
 	if(empty($result)) continue;
 	echo "<h2>$description</h2>";
 	echo $this->html->render_item_table($result);
