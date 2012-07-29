cdc31e71314eea7d9955b15bbb5cc0dc205d57d0
Thomas Mudrunka <tomas@mudrunka.cz>
2012-07-29 14:24:14 +0200

    SQL databaze s podporou locku

--- dump_sql.php
+++ dump_sql.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 if(isset($_SERVER['REQUEST_METHOD'])) die('Not for you');
-$nodump = array('item','model','barcode');
+$nodump = array('item','model','barcode','lock');
 require_once('sklad.conf.php');
 
 $ignore = '';
--- install.sql
+++ install.sql
@@ -230,7 +230,7 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-07-25 23:54:42
+-- Dump completed on 2012-07-29 14:23:53
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -306,7 +306,16 @@ CREATE TABLE `barcode` (
   PRIMARY KEY (`barcode_id`),
   KEY `model_id` (`model_id`),
   CONSTRAINT `barcode_ibfk_1` FOREIGN KEY (`model_id`) REFERENCES `model` (`model_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+DROP TABLE IF EXISTS `lock`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lock` (
+  `lock_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `lock_name` varchar(4096) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`lock_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
