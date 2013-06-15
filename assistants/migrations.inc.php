55f254c03684e67ef7c26e2157e3312f7836f86c
Thomas Mudrunka <tomas@mudrunka.cz>
2013-06-15 02:54:56 +0200

    Dokumentace migrace DB

--- assistants/migrations.inc.php
+++ assistants/migrations.inc.php
@@ -1,4 +1,8 @@
 <pre>
+<h1>-- Remove all uncountable items</h1>
+SELECT DISTINCT model_countable FROM model;
+DELETE item FROM item LEFT JOIN barcode USING(barcode_id) LEFT JOIN model USING(model_id) WHERE model_countable=0;
+UPDATE model SET model_countable=1;
 <h1>-- populate barcode table</h1><?php
 $data = $this->db->safe_query_fetch('SELECT model_id as id, model_barcode as bar FROM model;', false);
 //print_r($data);
